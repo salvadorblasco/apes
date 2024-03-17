a6774a31d0f43dc418d74c2143ecd13cd9ca9b2d
Salvador Blasco <salvador.blasco@protonmail.com>
2024-03-17 07:09:27 +0100

    more brigde development

--- src/bridge.py
+++ src/bridge.py
@@ -29,63 +29,90 @@ from datawidget import DataWidget
 
 
 class Bridge():
+    """Bridge between the GUI and the fitting engine.
+    """
     def __init__(self, model, titrationwidgets, datawidgets):
+        self.model = model                          # store these values to 
+        self.titrationwidgets = titrationwidgets    # update their values after
+        self.datawidgets = datawidgets              # the fitting
+
         self.jacobian_function = {}
         self.residual_function = {}
         #self.function_names = []
         self.function_size = []
-        self.variables = collections.OrderedDict()
-        self.input_variables = []
-        self.variable_shift = {}
-        self.variable_flags = {}
+        self.parameter = collections.OrderedDict()  # the values needed to calculate everything
+        self.parameter_flag = {}                    # the relation parameter/variable
+        self.variable = collections.OrderedDict()   # the parameters that are to be refined and
+                                                    # passed to the fitting routined
+        self.magnitude = collections.OrderedDict()  # the experimental values to fit upon
         self.variable_values = []
-        self.constraints = []
+        self.constraint = [None, None, None, None, None, None]
         self.stoichiometry = np.array(model.stoich)
         self.stoichiometryx = np.vstack((np.eye(model.number_components, dtype=int),
                                          self.stoichiometry))
-        self.model = model
         self.titrations = []
-        self.free_concentration = {}
-        self.analyticalc = {}
-        self.jacobian_parts = []
+        self.free_concentration: dict = {}   # key is the id of the titration associated
+                                             # value is the array of free concentration
+        self.analyticalc = {}                # key is the id of the titration associated
+        self.jacobian_part = []
+        self.jacobian_size = []
 
         # start collecting information
 
-        variables['beta'] = list(model.beta)        # it must be mutable
-        variable_flags['beta'] = model.beta_flags   # is should be immutable
+        # betas are always included first
+        parameter['beta'] = list(model.beta)        # it must be mutable
+        parameter_flag['beta'] = model.beta_flags   # is should be immutable
+        current_size: int = 0
+        for v, f in zip(model.beta, model.beta_flags):
+            if f == const.RF_CONSTANT:
+                pass
+            elif f == const.RF_REFINE:
+                current_size += 1
+                # TODO add value to variables
+            elif const.RF_CONSTRAINT1 <= f <= const.RF_CONSTRAINT6:
+                nconst = f - const.RF_CONSTRAINT1
+                if self.constraint[nconst] is None:
+                    current_size += 1
+                    self.constraint[nconst] = [v]
+                    # TODO add value to variables
+                else:
+                    self.constraint[nconst].append(v)
+            
         self.data_to_titration = {}
-        self.data_order = []
-        for dw in datawidgets:
-            if dw.name in self.data_order:
-                raise ValueError("Two datasets have the same name")
-            self.data_order.append(dw.name)
+        self.data_order = []    # the id of the datawidgets in order of appearance
+
+        for dw in datawidgets:                          # for each datawidget
+            self.data_order.append((id(dw), type(dw)))  # store the order of appearance, id and type
 
             match dw:
                 case CalorWidget():
                     raise NotImplementedError
                 case EmfWidget():
-                    self.variables[dw.name + '|E0'] = list(dw.emf0)      # it must be mutable
-                    self.variable_flags[dw.name] = dw.emf0_flags
+                    self.parameter[(id(name), 'emf0')] = list(dw.emf0)      # it must be mutable
+                    self.magnitudes[id(dw)] = np.array(dw.emf)              # it must be mutable
+                    self.parameter_flag[id(dw)] = dw.emf0_flags
                     if any(dw.emf0_flags):
-                        self.jacobian_parts.append(dw.name + '|emf0')
-                    self.data_to_titration[dw.name] = dw.titration_name
-                    self.jacobian_function[dw.name] = self.__emf_jacobian
-                    self.residual_function[dw.name] = self.__emd_residual
+                        self.jacobian_parts.append((id(dw), 'emf0'))
+                    self.data_to_titration[id(dw)] = dw.titration_name
+                    self.jacobian_function[id(dw)] = self.__emf_jacobian
+                    self.residual_function[id(dw)] = self.__emd_residual
                 case NmrWidget():
                     raise NotImplementedError
                 case SpecWidget():
                     raise NotImplementedError
 
         for tw in titrationwidgets:
-            self.titrations.append(tw.name)
-            self.variables[tw.name + '|init'] = list(tw.initial_amount)  # it must be mutable
-            self.variables[tw.name + '|buret'] = list(tw.buret)          # it must be mutable
-            self.variables[tw.name + '|v0'] = tw.starting_volume
-            self.variables[tw.name + '|titre'] = tw.titre
-            self.variable_flags[tw.name + '|init'] = tw.init_flags
+            self.titrations.append(id(tw))
+            self.variables[(id(tw, 'init')] = list(tw.initial_amount)  # it must be mutable
             if any(tw.init_flags):
-                self.build_jac_parts
-            self.variable_flags[tw.name + '|buret'] = tw.buret_flags
+                self.build_jac_parts.append((id(tw), 'init'))
+            self.variables[(id(tw, 'buret')] = list(tw.buret)          # it must be mutable
+            if any(tw.buret_flags):
+                self.build_jac_parts.append((id(tw), 'buret'))
+            self.variables[(id(tw, 'v0')] = tw.starting_volume
+            self.variables[(id(tw, 'titre')] = tw.titre
+            self.variable_flags[(id(tw), 'init')] = tw.init_flags
+            self.variable_flags[(id(tw), 'buret')] = tw.buret_flags
 
 
     def generate_jacobian(self):
@@ -97,25 +124,32 @@ class Bridge():
         |             |       |       |        |        |       |       |       ↑         ↑
         |    ∂E/∂β    |    0  |    0  |    0   |   1    | ∂E/∂t | ∂E/∂b | potentiometry   |
         |             |       |       |        |        |       |       |       ↓         |
-        +-------------+-------+-------+--------+--------+-------+-------+       +
+        +-------------+-------+-------+--------+--------+-------+-------+
         |             |       |       |        |        |       |       |       ↑       number
         |    ∂A/∂β    | ∂A/∂ε |    0  |    0   |   0    | ∂A/∂t | ∂A/∂b | spectrometry   of           
         |             |       |       |        |        |       |       |       ↓      magnitudes   
-        +-------------+-------+-------+--------+--------+-------+-------+       +       times        
+        +-------------+-------+-------+--------+--------+-------+-------+               times        
         |             |       |       |        |        |       |       |       ↑       number       
         |    ∂Q/∂β    |   0   |    0  | ∂Q/∂ΔH |   0    | ∂Q/∂t | ∂Q/∂b |  calorimetry   of           
         |             |       |       |        |        |       |       |       ↓    observations 
-        +-------------+-------+-------+--------+--------+-------+-------+       +         
+        +-------------+-------+-------+--------+--------+-------+-------+
         |             |       |       |        |        |       |       |       ↑         |
         |    ∂δ/∂β    |   0   | ∂δ/∂Δ |    0   |   0    | ∂δ/∂t | ∂δ/∂b |      NMR        |
         |             |       |       |        |        |       |       |       ↓         ↓
         +-------------+-------+-------+--------+--------+-------+-------+
-         ← -------------  number of variables to refine ------------- →
-
+         ← -------------  number of variables to refine -------------- →
         """
         def jacobian(values, free_concentration):
             amatrix = libeq.jacobian.amatrix(free_concentration, self.stoichiometryx)
-            ...
+            dlc_dlbeta = libeq.jacobian.dlogcdlogbeta(amatrix, free_concentration, stoichiometry):
+            for dataid, datatype in self.data_order:
+                # calculate the beta part
+                match datatype:
+                    case EmfWidget():           # calculate ∂E/∂β
+                        libemf.emf_jac_beta(dlc_dlbeta, beta, slope=1.0):
+                for jpart, var in self.jacobian_parts:
+                    # calculated the rest as needed
+                    ...
 
         return jacobian
 
@@ -151,6 +185,10 @@ class Bridge():
         return fconcs
 
     def update_variables(self, values):
+        # update the values in the widgets
+        #   self.model
+        #   self.titrationwidgets
+        #   self.datawidgets
         ...
 
     def weights(self):
